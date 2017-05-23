719a7dc8d1765000caad14e4303e47ef8e5006d8
Jacek Suchenia <jacek.suchenia@gmail.com>
2017-05-23 13:40:08 +0200

    Update name of one wrong metric

--- src/main/java/pl/suchenia/elasticsearchPrometheusMetrics/generators/IndicesMetricsGenerator.java
+++ src/main/java/pl/suchenia/elasticsearchPrometheusMetrics/generators/IndicesMetricsGenerator.java
@@ -21,7 +21,7 @@ public class IndicesMetricsGenerator implements  MetricsGenerator<NodeIndicesSta
         writer.addGauge("es_common_store_size")
                 .withHelp("Elasticsearch storage size (in bytes)")
                 .value(indicesStats.getStore().getSizeInBytes());
-        writer.addGauge("es_common_store_size")
+        writer.addGauge("es_throttle_time_millis")
                 .withHelp("Elasticsearch storage throttle time (in millis)")
                 .value(indicesStats.getStore().getThrottleTime().millis());
 
