ff0716d9d3ade88a6bfe7861dc98a3afededfddc
Jacek Suchenia <jacek.suchenia@ocado.com>
2017-07-31 19:46:31 +0200

    Support for ElasticSearch 5.5.0 and 5.5.1

--- src/test/java/pl/suchenia/elasticsearchPrometheusMetrics/PrometheusExporterPluginIT.java
+++ src/test/java/pl/suchenia/elasticsearchPrometheusMetrics/PrometheusExporterPluginIT.java
@@ -14,7 +14,7 @@ public class PrometheusExporterPluginIT extends ESClientYamlSuiteTestCase {
     }
 
     @ParametersFactory
-    public static Iterable<Object[]> parameters() throws IOException {
+    public static Iterable<Object[]> parameters() throws Exception {
         return ESClientYamlSuiteTestCase.createParameters();
     }
 }
